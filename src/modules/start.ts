ad774a651f6048c4a2e014f1ae715636546e1e17
erwin1234777 <erwinmkaufmann@gmail.com>
2021-06-03 17:53:58 -0300

    fixed dynamic event imports

--- src/modules/start.ts
+++ src/modules/start.ts
@@ -1,36 +1,35 @@
-import { CommandClient } from 'detritus-client/lib/commandclient';
+import { ClusterClient, CommandClient, GatewayClientEvents } from 'detritus-client';
 import * as config from '../config.json';
 import fs from 'fs';
 
 interface EventImport {
-  name: 'string';
-  execute(payload: any, attachment: any): void;
+  default: { name: 'string'; execute(payload: any, attachment: CommandClient): void };
 }
 
 // this is just cleaning stuff out of index.
-export default (client: CommandClient, startup: number) => {
+export default async (client: CommandClient, startup: number) => {
   // attach anything you want here to the client. for example your database driver
   // make sure you also add the type declaration in types.ts
   // client.postgres = new Pool(client.config.databaseUser);
   client.config = config;
 
   //reading all events in /events folder
-  const commandClientEvents = fs
-    .readdirSync('./src/events/commandClient')
-    .filter((file) => file.endsWith('.ts'));
+  const commandClientEvents = fs.readdirSync('./src/events/commandClient').filter(file => file.endsWith('.ts'));
   for (let file of commandClientEvents) {
     //importing events
-    import(`../events/commandClient/${file}`).then((ret: EventImport) => {
-      //binding the eventListeners
-      client.on(ret.name, (payload) => ret.execute(payload, client));
-    });
+    let ret: EventImport = await import(`../events/commandClient/${file}`);
+    // console.log(ret);
+    //binding the eventListeners
+    client.on(ret.default.name, payload => ret.default.execute(payload, client));
+
+    console.log(`Loaded event ${ret.default.name} to commandClient`);
   }
-  const clusterClientEvents = fs
-    .readdirSync('./src/events/clusterClient')
-    .filter((file) => file.endsWith('.ts'));
+
+  const clusterClientEvents = fs.readdirSync('./src/events/clusterClient').filter(file => file.endsWith('.ts'));
   for (let file of clusterClientEvents) {
-    import(`../events/clusterClient/${file}`).then((ret: EventImport) => {
-      client.client.on(ret.name, (payload) => ret.execute(payload, client));
-    });
+    let ret: EventImport = await import(`../events/clusterClient/${file}`);
+    // console.log(ret);
+    client.client.on(ret.default.name, payload => ret.default.execute(payload, client));
+    console.log(`Loaded event ${ret.default.name} to clusterClient`);
   }
 };
