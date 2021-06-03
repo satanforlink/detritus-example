5ed79921e8b05568db585d8d290305cbd4c120ed
erwin1234777 <erwinmkaufmann@gmail.com>
2021-06-03 17:54:11 -0300

    fixed wrong name in event

--- src/events/clusterClient/raw.ts
+++ src/events/clusterClient/raw.ts
@@ -1,11 +1,8 @@
 import { ClusterClient, GatewayClientEvents } from 'detritus-client';
 
 export default {
-  name: 'messageCreate',
-  async execute(
-    payload: GatewayClientEvents.Raw,
-    clusterClient: ClusterClient
-  ) {
+  name: 'raw',
+  async execute(payload: GatewayClientEvents.Raw, clusterClient: ClusterClient) {
     // do whatever you want here.
   },
 };
